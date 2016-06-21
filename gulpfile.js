cc17f8c91f5219d4b3453f12e2f7133286f4350e
zhun <zhun@huoli.com>
2016-06-21 18:01:23 +0800

    jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
   return gutil.log('Gulp is running');
@@ -7,4 +8,10 @@ gulp.task('default', function() {
 
 gulp.task('copyFile', function() {
   gulp.src('public/source/javascripts/courage.js').pipe(gulp.dest('public'));  
+});
+
+gulp.task('jshint', function() {
+  gulp.src('public/source/javascripts/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
 });
--- public/source/javascripts/courage.js
+++ public/source/javascripts/courage.js
@@ -1,1 +1,2 @@
 var courage = 'this is courage';
+dsada
