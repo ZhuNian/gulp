fc55de14d67a17b6d2ed50fc128197949ce09bb3
zhun <zhun@huoli.com>
2016-06-21 18:12:25 +0800

    scss to css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+var sass   = require('gulp-sass');
+var Path = require('path');
 
 gulp.task('default', function() {
   return gutil.log('Gulp is running');
@@ -14,4 +16,10 @@ gulp.task('jshint', function() {
   gulp.src('public/source/javascripts/**/*.js')
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('scss', function() {
+  gulp.src(Path.resolve(__dirname, 'public/stylesheets/*.scss'))
+      .pipe(sass())
+      .pipe(gulp.dest('public/stylesheets/css'))
 });
